c4356012f473972c5a83c948efcac9b22445686a
Jim Liu <hldh214@Gmail.com>
2017-07-13 22:40:56 +0800

    fix path 4 test

--- tests/test_kittens.py
+++ tests/test_kittens.py
@@ -6,7 +6,7 @@ from importlib import import_module
 
 class MyTestCase(unittest.TestCase):
     def test_kittens(self):
-        with open('../config.json') as fp:
+        with open('./config.json') as fp:
             config = json.load(fp, object_pairs_hook=OrderedDict)
             for key, value in config.items():
                 if not value['enable']:
