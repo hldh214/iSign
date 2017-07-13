46bbad94f1d8f8116a50105404bdb358ccb9ad4f
Jim Liu <hldh214@Gmail.com>
2017-07-13 21:45:45 +0800

    prettify main && README add schedule

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # iSign
 
 ## A record of my ugly code
+
+----------------
+
+## config.sample
+
+``` json
+{
+    "task_name": {
+        "schedule": {
+            "interval": 2,  // pause interval * unit between runs (default 1)
+            "unit": "hour",  // time units, e.g. 'minutes', 'hours', ...
+            "at_time": ""  // optional time at which this job runs
+        },
+        "enable": false,
+        "kitten": "tieba",  // in ./kittens dir
+        "config": {
+            ... // specific defined by each kitten
+        }
+    },
+    ...
+}
+```
--- iSign.py
+++ iSign.py
@@ -1,16 +1,22 @@
-from kittens import *
-from collections import OrderedDict
 import json
+import schedule
+import time
+from collections import OrderedDict
+from importlib import import_module
 
 with open('./config.json') as fp:
     config = json.load(fp, object_pairs_hook=OrderedDict)
     for key, value in config.items():
         if not value['enable']:
             continue
-        kitten = eval(value['kitten']).Kitten(value['config'])
-        res = kitten.meow()
 
-        if isinstance(res, list):
-            print([each.text for each in res])
-        else:
-            print(res.text)
+        kitten = import_module('kittens.{}'.format(value['kitten'])).Kitten(value['config'])
+        kitten_schedule = value['schedule']
+
+        getattr(
+            schedule.every(kitten_schedule['interval']), kitten_schedule['unit']
+        ).at(kitten_schedule['at_time']).do(kitten.meow)
+
+while True:
+    schedule.run_pending()
+    time.sleep(10)
