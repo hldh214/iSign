aff4c84b444e9c1745dc4ae687645269c3aa6cbe
Jim Liu <hldh214@Gmail.com>
2017-07-19 12:21:24 +0800

    use SafeScheduler

--- iSign.py
+++ iSign.py
@@ -4,9 +4,32 @@ from collections import OrderedDict
 from importlib import import_module
 from sys import argv
 
-import schedule
+# ref: https://gist.github.com/mplewis/8483f1c24f2d6259aef6
+import logging
+from traceback import format_exc
+import datetime
+
+from schedule import Scheduler
+
+logger = logging.getLogger('schedule')
+
+
+class SafeScheduler(Scheduler):
+    def __init__(self, reschedule_on_failure=True):
+        self.reschedule_on_failure = reschedule_on_failure
+        super().__init__()
+
+    def _run_job(self, job):
+        try:
+            super()._run_job(job)
+        except Exception:
+            logger.error(format_exc())
+            job.last_run = datetime.datetime.now()
+            job._schedule_next_run()
+
 
 with open(argv[1]) as fp:
+    schedule = SafeScheduler()
     config = json.load(fp, object_pairs_hook=OrderedDict)
     for key, value in config.items():
         if not value['enable']:
