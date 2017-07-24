af2ff2d9ecfcb52bd4509d12ce536471be9104e1
Jim Liu <hldh214@Gmail.com>
2017-07-24 21:24:55 +0800

    fix mobile qq `gen_bkn`

--- kittens/mobile_qq.py
+++ kittens/mobile_qq.py
@@ -3,13 +3,17 @@ import qqlib
 
 
 class Kitten:
-    bkn = 5381
-
     def __init__(self, config):
         qq = qqlib.QQ(config['usn'], config['pwd'])
         qq.login()
         self.usn = config['usn']
         self.skey = qq.session.cookies.get('skey')
+
+    def gen_bkn(self, skey, var_i=5381):
+        for each in skey:
+            var_i += self.int_overflow(var_i << 5) + ord(each)
+
+        return 2147483647 & var_i
 
     @staticmethod
     def int_overflow(val):
@@ -19,9 +23,6 @@ class Kitten:
         return val
 
     def meow(self):
-        for each in self.skey:
-            self.bkn += self.int_overflow(self.bkn << 5) + ord(each)
-
         headers = {
             'cookie': 'uin={0}; skey={1}'.format(self.usn, self.skey)
         }
@@ -35,7 +36,7 @@ class Kitten:
             # qq_qun_gift
             grequests.post(
                 'http://pay.qun.qq.com/cgi-bin/group_pay/good_feeds/draw_lucky_gift',
-                data='bkn={}'.format(self.bkn),
+                data='bkn={}'.format(self.gen_bkn(self.skey)),
                 headers=headers
             )
         ))
